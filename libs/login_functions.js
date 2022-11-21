411b5890e6629152d461eef72a03b0a225047935
samkhodak <samkhodak@gmail.com>
2022-11-20 23:46:43 -0800

    removed unnecessary code

---
+++ libs/login_functions.js
@@ -0,0 +1,83 @@
+const login_model = require("../models/login_schema.js");
+const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+
+const initialize_login = async (passport) => {
+  passport.use(
+    new localStrategy({ usernameField: "email" }, (email, password, done) => {
+      login_model.findOne({ email: email }, async (err, user) => {
+        if (err) {
+          console.log(err);
+          return done(err);
+        }
+        if (!user) {
+          console.log("No user with that email.");
+          return done(null, false, {
+            message: "No user found with that email.",
+          });
+        }
+
+        //Compare found password
+        const compared_pass = await bcrypt.compare(password, user.password);
+        if (!compared_pass) {
+          console.log("Incorrect pass");
+          return done(null, false, { message: "Incorrect password!" });
+        } else {
+          console.log("Successful");
+          return done(null, user);
+        }
+      });
+    })
+  );
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    login_model.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+};
+
+const redirect_not_auth = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  else res.redirect("/login");
+};
+
+const redirect_auth = (req, res, next) => {
+  if (req.isAuthenticated()) return res.redirect("/");
+  else return next();
+};
+
+const save_user = async (req, res) => {
+  try {
+    const existing_user = await login_model.find({ email: req.body.email });
+    if (!existing_user[0]) {
+      //if not, add them to the database
+      const pass_hash = await bcrypt.hash(req.body.password, 10);
+
+      const response = await login_model.create({
+        username: req.body.name,
+        email: req.body.email,
+        password: pass_hash,
+        uuid: uuidv4(),
+      });
+
+      console.log(`document that was added =>`, response);
+      res.redirect("/login");
+    }
+  } catch (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+};
+
+module.exports = {
+  initialize_login,
+  redirect_auth,
+  redirect_not_auth,
+  save_user,
+};
---
+++ libs/server_control.js
@@ -0,0 +1,19 @@
+const session = require('express-session');
+require("dotenv").config();
+
+const session_middleware = session({
+    secret: process.env.SESSION_SECRET,
+    credentials: true,
+    name: "sid",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      expires: 1000 * 60 * 60 * 24 * 2,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", //we only want secure and sameSite to be false/lax when we're making the app and testing.
+    },
+  });
+
+
+module.exports = session_middleware;
